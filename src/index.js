70c6fdc87c2e61de5e84ea3d6449b69741a45f45
Olga Berezka <olgakoltsova@Air-Olga.lan>
2023-02-12 21:15:03 +0000

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	console.log(str)
+	console.log(bracketsConfig)
+
+	let OPEN_BRACKETS = []
+	let BRACKETS_PAIR = {}
+	let stack = []
+
+	bracketsConfig.forEach(innerArr => {
+		OPEN_BRACKETS.push(innerArr[0])
+		BRACKETS_PAIR[innerArr[1]] = innerArr[0]
+	})
+
+	for (let i = 0; i < str.length; i++) {
+
+		let currentSymbol = str[i]
+
+		if (BRACKETS_PAIR[currentSymbol] === currentSymbol) {
+
+			let topElement = stack[stack.length - 1]
+
+			if (currentSymbol === topElement) {
+				stack.pop()
+			} else {
+				stack.push(currentSymbol)
+			}
+		}
+
+		else if (OPEN_BRACKETS.includes(currentSymbol)) {
+			stack.push(currentSymbol)
+
+		} else {
+
+			if (stack.length === 0) {
+				return false
+			}
+
+			let topElement = stack[stack.length - 1]
+
+			if (BRACKETS_PAIR[currentSymbol] === topElement) {
+				stack.pop()
+			}
+		}
+	}
+	return stack.length === 0
 }
